c57c7e1645589227a0f676fc2b98e76e65ed29f0
DanaKharaz <danakharaz@gmail.com>
2025-10-13 19:32:04 +0900

    finding neighbours done

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -10,7 +10,7 @@ trait Solver extends GameDef {
   /**
    * Returns `true` if the block `b` is at the final position
    */
-  def done(b: Block): Boolean = ???
+  def done(b: Block): Boolean = b == startBlock
 
   /**
    * This function takes two arguments: the current block `b` and
@@ -28,7 +28,10 @@ trait Solver extends GameDef {
    * It should only return valid neighbors, i.e. block positions
    * that are inside the terrain.
    */
-  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = ???
+  def neighborsWithHistory(b: Block, history: List[Move]): Stream[(Block, List[Move])] = {
+    val legalNeghbours = b.legalNeighbors
+    legalNeghbours.map((p: (Block, Move)) => (p._1, p._2 :: history)).toStream
+  }
 
   /**
    * This function returns the list of neighbors without the block
--- streams/src/test/scala/streams/BloxorzSuite.scala
+++ streams/src/test/scala/streams/BloxorzSuite.scala
@@ -53,6 +53,17 @@ class BloxorzSuite extends FunSuite {
     }
   }
 
+  test("neighbours with history for level 1") {
+    new Level1 {
+      val correct = Set(
+        (Block(Pos(1, 2), Pos(1, 3)), List(Right, Left, Up)),
+        (Block(Pos(2, 1), Pos(3, 1)), List(Down, Left, Up))
+      )
+      val got = neighborsWithHistory(Block(Pos(1, 1), Pos(1, 1)), List(Left, Up)).toSet
+      assert(correct == got)
+    }
+  }
+
   test("optimal solution for level 1") {
     new Level1 {
       assert(solve(solution) == Block(goal, goal))
