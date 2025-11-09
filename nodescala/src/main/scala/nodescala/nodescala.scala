184166271f350c55dc41906e43e56a537f645089
DanaKharaz <danakharaz@gmail.com>
2025-11-09 23:39:35 +0900

    implem respond and start

--- nodescala/src/main/scala/nodescala/nodescala.scala
+++ nodescala/src/main/scala/nodescala/nodescala.scala
@@ -29,7 +29,10 @@ trait NodeScala {
    *  @param token        the cancellation token
    *  @param body         the response to write back
    */
-  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = ???
+  private def respond(exchange: Exchange, token: CancellationToken, response: Response): Unit = {
+    while (!token.isCancelled && response.hasNext) exchange.write(response.next())
+    exchange.close()
+  }
 
   /** A server:
    *  1) creates and starts an http listener
@@ -41,7 +44,27 @@ trait NodeScala {
    *  @param handler        a function mapping a request to a response
    *  @return               a subscription that can stop the server and all its asynchronous operations *entirely*
    */
-  def start(relativePath: String)(handler: Request => Response): Subscription = ???
+  def start(relativePath: String)(handler: Request => Response): Subscription = {
+    val listener = createListener(relativePath)
+    val listenerSubscription = listener.start()
+
+    val serverSubscription = Future.run() { cancellationToken =>
+      def handleRequestsContinuously(): Future[Unit] = {
+        if (cancellationToken.isCancelled) Future.successful(())
+        else {
+          listener.nextRequest().flatMap {
+            case (request, exchange) => Future {
+              respond(exchange, cancellationToken, handler(request))
+            }.flatMap(_ => handleRequestsContinuously())
+          }
+        }
+      }
+
+      handleRequestsContinuously()
+    }
+
+    Subscription(listenerSubscription, serverSubscription)
+  }
 
 }
 
